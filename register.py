ce37b39a4931a08123e6927c9073471531c52f1a
جيرس <72812349+kira-xc@users.noreply.github.com>
2021-05-27 23:34:12 +0100

    Add files via upload

--- register.py
+++ register.py
@@ -1,33 +1,139 @@
 #!/usr/bin/python3
 #script by kira_xc
-#youtube : youtube.com/kiraxc
-#insta : kira_xc
+#youtube : https://www.youtube.com/kiraxc
+#instagram : kira_xc
 #github : kira-xc
-from os import _exit
+import json
+import base64
+import requests
+
+from uuid import UUID
+from os import urandom
+from time import timezone
+from typing import BinaryIO
+from binascii import hexlify
+from time import time as timestamp
+from locale import getdefaultlocale as locale
+
+from amino.lib.util import exceptions, headers, device, objects, helpers
+from amino.socket import Callbacks, SocketHandler
+import random,string,hashlib,base64,importlib
+def deviceaoss():
+    return str('01' + (hardwareInfo := hashlib.sha1(''.join(random.choices(string.ascii_uppercase + string.digits, k = 1000)).encode("utf-8"))).hexdigest() + hashlib.sha1(bytes.fromhex('01') + hardwareInfo.digest() + base64.b64decode("6a8tf0Meh6T4x7b0XvwEt+Xw6k8=")).hexdigest()).upper()
+device=deviceaoss()
+print("the new device id : ",device)
+from os import _exit,system
+def deviceido(device):
+    dvvv='{\n\"device_id\": \"'+device+'\", \n\"device_id_sig\": \"AaauX/ZA2gM3ozqk1U5j6ek89SMu\", \n\"user_agent\": \"Dalvik/2.1.0 (Linux; U; Android 7.1; LG-UK495 Build/MRA58K; com.narvii.amino.master/3.3.33180)\"\n}'
+    fdev=open("device.json","w+")
+    fdev.write(dvvv)
+    fdev.close()
+
+
 try:
     import amino
 except:
-    print("connect to network for run !  ")
+    print("connect to network for run !\or update the amino.py to last upadate  ")
+    deviceido(device)
+    amino.device.DeviceGenerator(deviceId=device)
     _exit(1)
+class newk(amino.Client):
+   
+    def register(self, nickname: str, email: str, password: str, deviceId: str = device):
+            """
+            Register an account.
+
+            **Parameters**
+                - **nickname** : Nickname of the account.
+                - **email** : Email of the account.
+                - **password** : Password of the account.
+                - **deviceId** : The device id being registered to.
+
+            **Returns**
+                - **Success** : 200 (int)
+
+                - **Fail** : :meth:`Exceptions <amino.lib.util.exceptions>`
+            """
+            data = json.dumps({
+                "secret": f"0 {password}",
+                "deviceID": deviceId,
+                "email": email,
+                "clientType": 100,
+                "nickname": nickname,
+                "latitude": 0,
+                "longitude": 0,
+                "address": None,
+                "clientCallbackURL": "narviiapp://relogin",
+                "type": 1,
+                "identity": email,
+                "timestamp": int(timestamp() * 1000)
+            })
+
+            response = requests.post(f"{self.api}/g/s/auth/register", data=data, headers=headers.Headers(data=data).headers, proxies=self.proxies, verify=self.certificatePath)
+            if response.status_code != 200: return exceptions.CheckException(json.loads(response.text))
+            else: return response.status_code
+        
+def renew():
+    class newk(amino.Client):
+   
+        def register(self, nickname: str, email: str, password: str, deviceId: str = device):
+                """
+                Register an account.
+
+                **Parameters**
+                    - **nickname** : Nickname of the account.
+                    - **email** : Email of the account.
+                    - **password** : Password of the account.
+                    - **deviceId** : The device id being registered to.
+
+                **Returns**
+                    - **Success** : 200 (int)
+
+                    - **Fail** : :meth:`Exceptions <amino.lib.util.exceptions>`
+                """
+                data = json.dumps({
+                    "secret": f"0 {password}",
+                    "deviceID": deviceId,
+                    "email": email,
+                    "clientType": 100,
+                    "nickname": nickname,
+                    "latitude": 0,
+                    "longitude": 0,
+                    "address": None,
+                    "clientCallbackURL": "narviiapp://relogin",
+                    "type": 1,
+                    "identity": email,
+                    "timestamp": int(timestamp() * 1000)
+                })
+
+                response = requests.post(f"{self.api}/g/s/auth/register", data=data, headers=headers.Headers(data=data).headers, proxies=self.proxies, verify=self.certificatePath)
+                if response.status_code != 200: return exceptions.CheckException(json.loads(response.text))
+                else: return response.status_code
+            
 from os import system
 from random import randint
 from ast import literal_eval
-client =amino.Client()
+
 password=input("your initial password : ")
 nicko=str(input("your name : "))
-try:
-    deviceids=open("deviceid.txt")
-    deviceId=str(deviceids.readline()).strip()
-    df=True
-    print("\"deviceid.txt\" yes found 👌")
-except:
-    df=False
-    deviceId='01B7AA1CF76DE9FCF8E11AC71E62B2FE54D66E376637B0C9FC4A50D1115FAD5F8F27C7ED2CF28DBCE0'
+cpt3=0
 while True:
+    importlib.reload(amino)
+    deviceido(device)
+    amino.device.DeviceGenerator(deviceId=device)
+    renew()
+    amino.device.DeviceGenerator(deviceId=device)
+    deviceido(device)
+    if cpt3==3:
+        device=deviceaoss()
+        cpt3=0
+    cpt3+=1
+    client=newk(deviceId=device)
+    client.device_id=device
     nickname=nicko+str(randint(1,99999999))
     email=input("give me \"email\" for new sign up  : ")
     try:
-        z=client.register(nickname=nickname, email=email, password=password, deviceId=deviceId)
+        z=client.register(nickname=nickname, email=email, password=password, deviceId=device)
     except Exception as e:
         z=0
         if hasattr(e, 'message'):
@@ -40,16 +146,9 @@ while True:
             print("\n\n\n"+str(messo)+"\n\n\n")
             dc=literal_eval(mess)
             if dc.get("api:statuscode")==219:
-                if df==True:
-                    deviceId=(deviceids.readline()).strip()
-                    if deviceId!="\n" and deviceId!="":
-                        print ("new device id :" +str(deviceId))
-                    else:
-                        print("\nrenew your deviceid.txt file")
-                        _exit(1)
-                else:
-                    print("\n \n\"deviceid.txt\" not faund ")
-                    _exit(1)
+                device=deviceaoss()
+                cpt3=0
+                print("the device id changed to : ",device)
         continuee=input("to by continue y/n : ")
         if continuee=="n":
             _exit(1)
@@ -77,4 +176,5 @@ while True:
         print("\n\n\n")
         saveemail="echo "+email+">>email.txt"
         system(saveemail)
+    
     
